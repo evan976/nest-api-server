e157f5770a35683c0405274a370031e69336873b
wujihua <wjh50940@163.com>
2022-03-15 13:41:12 +0800

    feat: comment entity

---
+++ src/interface/app.interface.ts
@@ -0,0 +1,12 @@
+export type Location = {
+  country: string
+  province: string
+  city: string
+}
+
+export type Author = {
+  name: string
+  email: string
+  site: string
+  location: Location
+}
---
+++ src/module/comment/comment.entity.ts
@@ -0,0 +1,69 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { ApiProperty } from '@nestjs/swagger'
+import { Author } from '@/interface/app.interface'
+import { CommentState, Weights } from '@/interface/state.interface'
+
+@Entity()
+export class Comment {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @ApiProperty()
+  @Column()
+  author: Author
+
+  @ApiProperty()
+  @Column({ type: 'mediumtext', default: null })
+  content: string
+
+  @ApiProperty()
+  @Column({ type: 'mediumtext', default: null })
+  html: string
+
+  @ApiProperty()
+  @Column()
+  postId: string
+
+  @ApiProperty()
+  @Column()
+  url: string
+
+  @ApiProperty()
+  @Column({ default: null })
+  parentId: string
+
+  @ApiProperty()
+  @Column({ type: 'mediumtext', default: null })
+  userAgent: string
+
+  @ApiProperty()
+  @Column('simple-enum', { enum: CommentState, default: 1 })
+  status: number
+
+  @ApiProperty()
+  @Column('simple-enum', { enum: Weights, default: 1 })
+  weight: number
+
+  @ApiProperty()
+  @CreateDateColumn({
+    type: 'datetime',
+    comment: '创建时间',
+    name: 'created_at',
+  })
+  createdAt: Date
+
+  @ApiProperty()
+  @UpdateDateColumn({
+    type: 'datetime',
+    comment: '更新时间',
+    name: 'updated_at',
+  })
+  updatedAt: Date
+}
