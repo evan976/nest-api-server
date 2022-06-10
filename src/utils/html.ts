d0e4e473ecd65e6227db3a1bd79f865927505e11
wujihua <wjh50940@163.com>
2022-06-10 10:15:53 +0800

    fix: mail format

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -6,11 +6,11 @@ export const getNewCommentHtml = (
 ) => {
   return `
     <div style="color: #40485B; font-size: 14px; background-color: #f8f8f8;">
-      <div style="width: 100%; max-width: 600px; padding: 20px; margin: 0 auto;">
-        <div style="text-align: center;margin-bottom: 20px;">
+      <div style="max-width: 600px; padding: 20px; margin: 0 auto;">
+        <div style="text-align: center;margin-bottom: 20px; width: 100%">
           <img src="https://static.evanone.site/1634980227946" height="30" alt="Logo black">
         </div>
-        <div style="background: #fff; border: 1px solid #E3E9ED; margin-bottom: 10px;">
+        <div style="width: 100%; background: #fff; border: 1px solid #E3E9ED; margin-bottom: 10px;">
           <div style="font-size: 18px; line-height: 30px; padding: 10px 20px; background: #fcfcfc; border-bottom: 1px solid #E3E9ED;">博客评论通知</div>
           <div style="padding: 20px;line-height: 1.7;">
             <p>
@@ -23,7 +23,7 @@ export const getNewCommentHtml = (
           </div>
         </div>
         <div style="color: #9B9B9B; font-size: 12px; margin-top: 20px;">
-          @evanone.site
+          <a href="https://evanone.site">@evanone.site</a>
         </div>
       </div>
     </div>
@@ -38,11 +38,11 @@ export const getReplyCommentHtml = (
 ) => {
   return `
     <div style="color: #40485B; font-size: 14px; background-color: #f8f8f8;">
-      <div style="width: 100%; max-width: 600px; padding: 20px; margin: 0 auto;">
-        <div style="text-align: center;margin-bottom: 20px;">
+      <div style="max-width: 600px; padding: 20px; margin: 0 auto;">
+        <div style="text-align: center;margin-bottom: 20px; width: 100%">
           <img src="https://static.evanone.site/1634980227946" height="30" alt="Logo black">
         </div>
-        <div style="background: #fff; border: 1px solid #E3E9ED; margin-bottom: 10px;">
+        <div style="width: 100%; background: #fff; border: 1px solid #E3E9ED; margin-bottom: 10px;">
           <div style="font-size: 18px; line-height: 30px; padding: 10px 20px; background: #fcfcfc; border-bottom: 1px solid #E3E9ED;">博客回复通知</div>
           <div style="padding: 20px;line-height: 1.7;">
             <p>
@@ -55,7 +55,7 @@ export const getReplyCommentHtml = (
           </div>
         </div>
         <div style="color: #9B9B9B; font-size: 12px; margin-top: 20px;">
-          @evanone.site
+          <a href="https://evanone.site">@evanone.site</a>
         </div>
       </div>
     </div>
